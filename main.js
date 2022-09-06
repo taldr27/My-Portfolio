65681e9c60fe59fe0e15fb05cb3eee0cf8112c20
taldr27 <diegogarciaq98@gmail.com>
2022-09-05 19:09:57 -0500

    Add script tag at the bottom of the page

--- main.js
+++ main.js
@@ -1,2 +1,1 @@
-
 function myFunction() { const x = document.getElementById('hide-menu'); if (x.style.display === 'flex') { x.style.display = 'none'; } else { x.style.display = 'flex'; } }document.getElementById('burger').onclick = myFunction();
