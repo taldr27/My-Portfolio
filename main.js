695989ebfbe6f6af6fa24379df4ea3747f25a78a
taldr27 <diegogarciaq98@gmail.com>
2022-09-06 11:40:19 -0500

    Add popup window for projects section

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-function myFunction() { const x = document.getElementById('hide-menu'); if (x.style.display === 'none') { x.style.display = 'flex'; } else { x.style.display = 'none'; } }document.getElementById('burger').onclick = myFunction();
+function toggleBar() {
+  const x = document.getElementById('hide-menu');
+  if (x.style.display === 'none') {
+    x.style.display = 'flex';
+  }
+  else {
+    x.style.display = 'none';
+  }
+}
+
+document.getElementById('burger').addEventListener('click', toggleBar);
+
+var projects = [project1, project2, project3];
+
+function projects(title, image, description){
+    this.title = title;
+}
+
+const project1 = {
+  title: 'First Project',
+  
+};
