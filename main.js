be0273942ed93b69112d15f683cc19b390c84882
taldr27 <diegogarciaq98@gmail.com>
2022-09-05 19:15:30 -0500

    Add script tag at the bottom of the page

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-function myFunction() { const x = document.getElementById('hide-menu'); if (x.style.display === 'flex') { x.style.display = 'none'; } else { x.style.display = 'flex'; } }document.getElementById('burger').onclick = myFunction();
+function myFunction() { const x = document.getElementById('hide-menu'); if (x.style.display === 'none') { x.style.display = 'flex'; } else { x.style.display = 'none'; } }document.getElementById('burger').onclick = myFunction();
