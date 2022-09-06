171659540c14f7bca1ea0fb9ae5c38e31dad6904
IngDavidTM <david-1011@hotmail.com>
2022-09-06 12:36:11 -0500

    add script

--- main.js
+++ main.js
@@ -10,13 +10,26 @@ function toggleBar() {
 
 document.getElementById('burger').addEventListener('click', toggleBar);
 
-var projects = [project1, project2, project3];
-
-function projects(title, image, description){
-    this.title = title;
+function toggleBarWorks() {
+  const y = document.getElementById('mobile-popup');
+  if (y.style.display === 'none') {
+    y.style.display = 'flex';
+}
+  else {
+    y.style.display = 'none';
+  }
 }
 
-const project1 = {
-  title: 'First Project',
+const boxes = document.querySelectorAll('.button');
+boxes.forEach((box) => { box.addEventListener('click', toggleBarWorks); });
+
+// var projects = [project1, project2, project3];
+
+// function projects(title, image, description){
+//     this.title = title;
+// }
+
+// const project1 = {
+//   title: 'First Project',
   
-};
+// };
